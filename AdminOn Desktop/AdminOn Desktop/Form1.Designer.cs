aae64f9864a0ee6c81a6b7aaa4f384359efd7265
lupearobert <contact@lupearobert.ro>
2019-09-27 14:58:23 +0300

    Am creat meniul de sus

--- AdminOn Desktop/AdminOn Desktop/Form1.Designer.cs
+++ AdminOn Desktop/AdminOn Desktop/Form1.Designer.cs
@@ -28,20 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adaugaOAsociatieNouaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dashBoarCentralizatoareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitIesireToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.venituriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cheltuieliToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bancaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaDeCheltuieliToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rapoarteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.despreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.venituriToolStripMenuItem,
+            this.cheltuieliToolStripMenuItem,
+            this.bancaToolStripMenuItem,
+            this.listaDeCheltuieliToolStripMenuItem,
+            this.rapoarteToolStripMenuItem,
+            this.despreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(515, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adaugaOAsociatieNouaToolStripMenuItem,
+            this.dashBoarCentralizatoareToolStripMenuItem,
+            this.exitIesireToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // adaugaOAsociatieNouaToolStripMenuItem
+            // 
+            this.adaugaOAsociatieNouaToolStripMenuItem.Name = "adaugaOAsociatieNouaToolStripMenuItem";
+            this.adaugaOAsociatieNouaToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.adaugaOAsociatieNouaToolStripMenuItem.Text = "Adauga o Asociatie Noua";
+            // 
+            // dashBoarCentralizatoareToolStripMenuItem
+            // 
+            this.dashBoarCentralizatoareToolStripMenuItem.Name = "dashBoarCentralizatoareToolStripMenuItem";
+            this.dashBoarCentralizatoareToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.dashBoarCentralizatoareToolStripMenuItem.Text = "DashBoar / Centralizatoare";
+            // 
+            // exitIesireToolStripMenuItem
+            // 
+            this.exitIesireToolStripMenuItem.Name = "exitIesireToolStripMenuItem";
+            this.exitIesireToolStripMenuItem.Size = new System.Drawing.Size(214, 22);
+            this.exitIesireToolStripMenuItem.Text = "Exit / Iesire";
+            // 
+            // venituriToolStripMenuItem
+            // 
+            this.venituriToolStripMenuItem.Name = "venituriToolStripMenuItem";
+            this.venituriToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.venituriToolStripMenuItem.Text = "Venituri";
+            // 
+            // cheltuieliToolStripMenuItem
+            // 
+            this.cheltuieliToolStripMenuItem.Name = "cheltuieliToolStripMenuItem";
+            this.cheltuieliToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.cheltuieliToolStripMenuItem.Text = "Cheltuieli";
+            // 
+            // bancaToolStripMenuItem
+            // 
+            this.bancaToolStripMenuItem.Name = "bancaToolStripMenuItem";
+            this.bancaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.bancaToolStripMenuItem.Text = "Banca";
+            // 
+            // listaDeCheltuieliToolStripMenuItem
+            // 
+            this.listaDeCheltuieliToolStripMenuItem.Name = "listaDeCheltuieliToolStripMenuItem";
+            this.listaDeCheltuieliToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.listaDeCheltuieliToolStripMenuItem.Text = "Lista de Cheltuieli";
+            // 
+            // rapoarteToolStripMenuItem
+            // 
+            this.rapoarteToolStripMenuItem.Name = "rapoarteToolStripMenuItem";
+            this.rapoarteToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.rapoarteToolStripMenuItem.Text = "Rapoarte";
+            // 
+            // despreToolStripMenuItem
+            // 
+            this.despreToolStripMenuItem.Name = "despreToolStripMenuItem";
+            this.despreToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.despreToolStripMenuItem.Text = "Despre";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(515, 391);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adaugaOAsociatieNouaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dashBoarCentralizatoareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitIesireToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem venituriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cheltuieliToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bancaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaDeCheltuieliToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rapoarteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem despreToolStripMenuItem;
     }
 }
 
